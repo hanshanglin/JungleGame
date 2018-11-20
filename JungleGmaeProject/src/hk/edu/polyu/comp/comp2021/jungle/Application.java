bb55c6dd90e69af494f8226ab6fe129d25e4ae5d
user19990313 <31213599+user19990313@users.noreply.github.com>
2018-11-20 11:20:01 +0800

    Added Javadoc

--- JungleGmaeProject/src/hk/edu/polyu/comp/comp2021/jungle/Application.java
+++ JungleGmaeProject/src/hk/edu/polyu/comp/comp2021/jungle/Application.java
@@ -4,9 +4,15 @@ import hk.edu.polyu.comp.comp2021.jungle.model.Controller;
 import hk.edu.polyu.comp.comp2021.jungle.model.HybridScanner;
 import hk.edu.polyu.comp.comp2021.jungle.ui.UIController;
 
-
+/**
+ * start of the program
+ */
 public class Application {
 
+    /**
+     * new a controller and setup game
+     * @param args no args need to input
+     */
     public static void main(String[] args){
         Controller controller = new Controller();
 
