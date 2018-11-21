c5ce830d23aa7c473d8f21c314334df3115d5cb8
user19990313 <31213599+user19990313@users.noreply.github.com>
2018-11-21 22:42:32 +0800

    Added Javadoc

--- JungleGameProject/src/hk/edu/polyu/comp/comp2021/jungle/controller/HybridScanner.java
+++ JungleGameProject/src/hk/edu/polyu/comp/comp2021/jungle/controller/HybridScanner.java
@@ -5,13 +5,26 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
+/**
+ *
+ * A multi-source concurrent input handler that mixes both System.in and explicitly inserted strings as one stream.
+ * String feeding is always unblocked while fetching will block if stream cache is empty.
+ * Enables one-way UI-to-Controller communication and solves main thread awaiting input deadlock.
+ */
 public class HybridScanner{
 
+    /**
+     * Singleton
+     */
     public static final HybridScanner instance=new HybridScanner();
 
     private BufferedReader sc;
     private ConcurrentLinkedQueue<String> queue;
 
+    /**
+     *
+     * Daemon thread that reads and caches every lines of inputs from System.in
+     */
     private class ScannerSubroutine extends Thread{
         private BufferedReader sc;
         private HybridScanner parent;
@@ -30,13 +43,17 @@ public class HybridScanner{
                     parent.feed(s);
             }
             }catch(IOException e){
-
+                return;
             }
         }
     }
 
     private ScannerSubroutine subroutine;
 
+    /**
+     *
+     * constructor
+     */
     public HybridScanner(){
         this.sc=new BufferedReader(new InputStreamReader(System.in));
         this.queue=new ConcurrentLinkedQueue<String>();
@@ -44,6 +61,12 @@ public class HybridScanner{
         this.subroutine.start();
     }
 
+    /**
+     *
+     * gets a line from input cache.
+     * method is blocking caller thread until the cache is not empty
+     * @return the content
+     */
     public String nextLine(){
         while(true){
             if(!queue.isEmpty())
@@ -51,6 +74,12 @@ public class HybridScanner{
         }
     }
 
+    /**
+     *
+     * caches a string to the end of the input queue.
+     * method is operating synchronized object and will never block the thread.
+     * @param s the string
+     */
     public void feed(String s){
         queue.add(s);
     }
